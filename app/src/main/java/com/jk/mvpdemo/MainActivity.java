946b92efa4eb3e0bdcc22a451b57785f7aeea650
JK <1115170755@qq.com>
2020-09-27 08:11:10 +0800

    2020.09.27 08:10 git push

--- app/src/main/java/com/jk/mvpdemo/MainActivity.java
+++ app/src/main/java/com/jk/mvpdemo/MainActivity.java
@@ -1,17 +1,40 @@
 package com.jk.mvpdemo;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        //test
-        //test2
+        tvTitle.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_title:
+                Log.e("123","This is a MVP Demo");
+                Toast.makeText(MainActivity.this,"This is a MVP Demo",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
